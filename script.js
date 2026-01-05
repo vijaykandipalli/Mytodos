5932b28f428f30e918a4974e3672d551f1ba63ae
vizzu999 <vijayshankarkandipalli@gmail.com>
2026-01-05 19:34:28 +0530

    add eventlisteners for add button

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const todoList = document.getElementById("todo-list");
 const taskCount = document.getElementById("task-count");
 const emptyState = document.querySelector(".empty-state");
 const filterBtns = document.querySelectorAll(".filter");
+
+addBtn.addEventListener("click", addTask);
+
+taskInput.addEventListener("keydown", e => {
+    if (e.key === "Enter") addTask();
+});
