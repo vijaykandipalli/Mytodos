6535c8c4bcc13aec0baa1b2fe824cc8fbbd60868
vizzu999 <vijayshankarkandipalli@gmail.com>
2026-01-05 19:32:53 +0530

    initialize Dom elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const taskInput = document.getElementById("task-input");
+const addBtn = document.getElementById("add-btn");
+const todoList = document.getElementById("todo-list");
+const taskCount = document.getElementById("task-count");
+const emptyState = document.querySelector(".empty-state");
+const filterBtns = document.querySelectorAll(".filter");
